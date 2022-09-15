e0ab011ef34b1de89abc2397faa143bfb3787ba5
Denis Osinsky <osinsky@gmail.com>
2022-09-15 16:30:41 +0700

    add config examples

---
+++ config/mongodb.example.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'class' => '\yii\mongodb\Connection',
+    'dsn' => 'mongodb://yii2-talking-heads-mongodb-1:27017/talking_heads',
+    // Schema cache options (for production environment)
+    // 'enableSchemaCache' => true,
+    // 'schemaCacheDuration' => 60,
+    // 'schemaCache' => 'cache',
+];
